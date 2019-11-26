feb10dcff865abec45b3f44a44fdda728b6edf8f
Michael Zhang <michaelzhang6284@gmail.com>
2019-11-26 16:01:47 -0500

    first commit

---
+++ Eventmi/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  Eventmi
+//
+//  Created by Michael Zhang on 11/26/19.
+//  Copyright © 2019 Michael Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("hello")
+                .font(.largeTitle)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+            Text("Third View")
+            .font(.title)
+            .tabItem {
+                VStack {
+                    Image("second")
+                    Text("Third")
+                }
+            }
+            .tag(2)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
