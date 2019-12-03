b83e3cbdd0be39e225604f52e63123b18763f0ba
Michael Zhang <michaelzhang6284@gmail.com>
2019-12-03 14:57:00 -0500

    added styling to event creation form

--- Eventmi/Views/CreateEventForm.swift
+++ Eventmi/Views/CreateEventForm.swift
@@ -9,11 +9,30 @@
 import SwiftUI
 
 struct CreateEventForm: View {
+    let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
     var body: some View {
-        
-        HStack {
+        VStack {
+            Text("Create Event")
+                .font(.title)
             TextField("Group name", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-            
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding()
+            DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { Text("Date") })
+            TextField("Location", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding()
+            GroupInviteView()
+            TextField("Add a description for your event?", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .frame(width: 350, height: 100)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding()
         }
     }
 }
