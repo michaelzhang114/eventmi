ff4c24206dd2cca567aa6482a63bf9a9160a867a
aletheatoh <alethea.toh@duke.edu>
2019-12-05 16:59:39 -0500

    log in

---
+++ Eventmi/Views/Pages/LoginPage.swift
@@ -0,0 +1,49 @@
+//
+//  LoginPage.swift
+//  Eventmi
+//
+//  Created by Alethea Toh on 12/5/19.
+//  Copyright © 2019 Michael Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+    
+    var body: some View {
+        
+        VStack {
+            Image("eventmi-logo")
+            .resizable()
+            .shadow(radius: 10)
+            .frame(width: 300.0, height:240)
+            
+            VStack {
+                TextField("Phone Number", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding()
+                
+                TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Log In")
+                }
+            }
+        }
+        
+        
+    }
+}
+
+struct LoginPage_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginPage()
+    }
+}
